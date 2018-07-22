c9513713016be621bdee2e3d77922cc415569fc0
Wilkins Liang <wilkinsweiqiangliang@gmail.com>
2018-07-22 22:25:43 +1000

    dont need since we move to real database

--- server/src/index.js
+++ server/src/index.js
@@ -1,13 +1,5 @@
 const {GraphQLServer} = require('graphql-yoga');
 
-let links = [{
-    id: 'link-0',
-    url: 'www.howtographql.com',
-    description: 'Fullstack tutorial for GraphQL'
-  }]
-
-// temporary solution for id auto increasement
-let idCount = links.length;
 // 
 // The resolvers object is the actual implementation of the GraphQL schema.
 //  Notice how its structure is identical to the structure of the type definition inside typeDefs: Query.info.
