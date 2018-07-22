f111cc9a6cf9214bffccddf54ce3d55f3946c9fb
Wilkins Liang <wilkinsweiqiangliang@gmail.com>
2018-07-22 17:18:28 +1000

    added mutation type for graphql to create new link

--- server/src/index.js
+++ server/src/index.js
@@ -6,6 +6,8 @@ let links = [{
     description: 'Fullstack tutorial for GraphQL'
   }]
 
+// temporary solution for id auto increasement
+let idCount = links.length;
 // 
 // The resolvers object is the actual implementation of the GraphQL schema.
 //  Notice how its structure is identical to the structure of the type definition inside typeDefs: Query.info.
@@ -14,11 +16,17 @@ const resolvers = {
         info: () => `This is the API of a Hackernews Clone`,
         feed: () => links
     },
-    Link: {
-        id: (root) => root.id,
-        description: (root) => root.description,
-        url: (root) => root.url
-    }
+    Mutation: {
+        post: (root,args) => {
+            const link = {
+                id: `link-${idCount++}`,
+                description: args.description,
+                url: args.url
+            }
+            links.push(link);
+            return link;
+        }
+    },
 }
 
 // 
